aa7f56b49c8c6f550e686aeaefec5ef685699785
Francisco Gonçcalves <u1000d100pre@gmail.com>
2025-09-03 21:35:36 -0300

    inserindo mais variaveis (altura,calcular,exibir frase)

--- Python/Fundamentos/exemplo.py
+++ Python/Fundamentos/exemplo.py
@@ -2,3 +2,7 @@ nome = "Arthur Souza"
 print(nome)
 idade = 20
 print(idade)
+altura = 1.80
+calcular = idade * altura
+print(calcular)
+print(nome, "tem", idade, "anos e mede", altura, "de altura.")
